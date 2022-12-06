280ad2023dd4ded0b9deceb8b67bb7df6f08e8df
CavidMemmedli9 <javidsm@code.edu.az>
2022-12-06 12:44:24 +0400

    second

---
+++ home.js
@@ -0,0 +1,25 @@
+$(function() {
+    $(".imgDiv").vegas({
+        delay:4000,
+        slides:[
+            {src:"https://fiorello.qodeinteractive.com/wp-content/uploads/2018/05/h3-slider-background.jpg"},
+            {src:"https://fiorello.qodeinteractive.com/wp-content/uploads/2018/05/h3-slider-background-2.jpg"},
+            {src:"https://fiorello.qodeinteractive.com/wp-content/uploads/2018/05/h3-slider-background-3.jpg"}
+         ],
+        overlay:"overlays/01.png",
+        animation:"kenburnsUp"
+    })
+});
+
+$(function() {
+    $(".word").vegas({
+        delay:4000,
+        slides:[
+            {src:"https://fiorello.qodeinteractive.com/wp-content/uploads/2018/05/shop-4-img.jpg"},
+            {src:"https://fiorello.qodeinteractive.com/wp-content/uploads/2018/05/port-img-1-845x650.jpg"},
+            {src:"https://fiorello.qodeinteractive.com/wp-content/uploads/2018/04/shop-13-img.jpg"}
+         ],
+        overlay:"overlays/01.png",
+        animation:"kenburnsUp"
+    })
+});
