9f13fbd38d298ed7510c483c4e7d0445f71b73dd
CavidMemmedli9 <113415691+CavidMemmedli9@users.noreply.github.com>
2022-12-10 00:34:44 +0400

    second page completed

--- shop-list.js
+++ shop-list.js
@@ -1,17 +1,16 @@
 $('.owl-carousel').owlCarousel({
-    stagePadding: 20,
     loop:true,
-    margin:10,
+    margin:30,
     nav:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:2
         },
         1000:{
-            items:5
+            items:4
         }
     }
 })
