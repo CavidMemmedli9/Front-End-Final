e627c1b07d747ab724e086181c0a623a422995fd
Mammadli Javid <JavidSM@code.local>
2022-12-11 18:11:02 +0400

    accordion

---
+++ accordion.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("#p").click(function(){
+        $("#div").fadeToggle();
+        
+    })
+})
+
+$(document).ready(function(){
+    $("#p1").click(function(){
+        $("#div2").fadeToggle();
+        
+    })
+})
+$(document).ready(function(){
+    $("#p2").click(function(){
+        $("#div3").fadeToggle();
+        
+    })
+})
